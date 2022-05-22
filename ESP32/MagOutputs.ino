fdfdccc5d62b6cadc382652239a7a72a888fe74b
zoe liu <zoeliuziwei@gmail.com>
2022-05-22 16:12:32 -0400

    tested

--- ESP32/MagOutputs.ino
+++ ESP32/MagOutputs.ino
@@ -1,44 +1,68 @@
-
-
-
-#include <Adafruit_FXOS8700.h>
 #include <Wire.h>
+#include <Adafruit_FXOS8700.h>
 #include <Adafruit_Sensor.h>
 
+const int HANDSHAKE = 0;
+const int STREAM = 1;
+
+//time between data acquisition 
+int daqDelay = 100; //100ms 10hz
+int inByte;
 /* Assign a unique ID to this sensor at the same time */
 Adafruit_FXOS8700 accelmag = Adafruit_FXOS8700(0x8700A, 0x8700B);
 
+void printMag() 
+{
+  // Using arduino sensor libraries to get accel and mag readings
+  sensors_event_t AccelEvent, MagEvent;
+  accelmag.getEvent(&AccelEvent,&MagEvent);
+  // Write the results in serial port 
+  if (Serial.availableForWrite())
+  {
+    Serial.print(MagEvent.magnetic.x, 6);
+    Serial.print(",");
+    Serial.print(MagEvent.magnetic.y, 6);
+    Serial.print(",");
+    Serial.println(MagEvent.magnetic.z, 6);
+  }
+}
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   
   //Wait for serial monitor 
-  while (!Serial){
+  while (!Serial)
+  {
     delay(1);
   }
-
-  Serial.println("FXOS8700 Test");
+  
   //initialize sensors 
-  if (!accelmag.begin()) {
+  Serial.println("FXOS8700 Test");
+  if (!accelmag.begin()) 
+  {
     Serial.println("No FXOS8700 detected. Check connections");
     while (1);
   }
   
 }
 
-void loop() {
-  sensors_event_t AccelEvent, MagEvent;
+void loop()
+{
 
-  accelmag.getEvent(&AccelEvent,&MagEvent);
-  
-  Serial.print(MagEvent.magnetic.x, 6);
-  Serial.print(",");
-  Serial.print(MagEvent.magnetic.y, 6);
-  Serial.print(",");
-  Serial.println(MagEvent.magnetic.z, 6);
-
-  delay(100);
-  
-
-  
-
+  if (Serial.available() > 0)
+  {
+    inByte = Serial.read();
+    switch (inByte) 
+    {
+      case HANDSHAKE:
+        if (Serial.availableForWrite())
+        {
+          Serial.println("Message received");
+        }
+        break;
+      case STREAM:
+        printMag();
+        break;
+    }
+  }
 }
--- MagLog.py
+++ MagLog.py
@@ -4,6 +4,10 @@ import numpy as np
 import serial 
 import serial.tools.list_ports
 
+HANDSHAKE = 0
+STREAM = 1
+timeout = 1
+
 def find_ESP32(port=None):
     """find serial port connected to the ESP32"""
     if port is None:
@@ -13,9 +17,38 @@ def find_ESP32(port=None):
                 ESP32Port = port.device
     return ESP32Port
 
+def handshake_ESP32(ESP32, sleep_time =1, print_message = True, handshake_code = 0):
+    """testing connections with ESP32"""
+    #initializing
+    ESP32.close()
+    ESP32.open()
+    time.sleep(sleep_time)
+
+    #set long timeout to establish handshake 
+    timeout = ESP32.timeout
+    ESP32.timeout = 2
+
+
+    #discard junk in the input buffer 
+    _ = ESP32.read_all()
+
+    #request 
+    ESP32.write(bytes([handshake_code]))
+    #read
+    handshake_message = ESP32.read_until()
+    #repeat
+    ESP32.write(bytes([handshake_code]))
+    handshake_message = ESP32.read_until()
+
+    #optional to print the handshake message
+    if print_message:
+        print("Handshake established: " + handshake_message.decode())
+    #reset timeout
+    # ESP32.timeout = timeout
+    return 
+
 def dataParser(raw):
     raw = raw.decode()
-
     if raw[-1] != "\n":
         raise ValueError(
             "Input must end with newline, otherwise message is incomplete."
@@ -25,39 +58,50 @@ def dataParser(raw):
 
 def dataStream(ESP32):
     SAMPLE_FREQ = 10 #hz
-    SAMPLE_TIME = 5 #s
+    SAMPLE_TIME = 3 #s
+    sample_delay = 1/SAMPLE_FREQ
     N = int(SAMPLE_FREQ*SAMPLE_TIME)
     #Initialize output
     measurements = np.zeros((N, 3), dtype='float')
-
+    #turn on stream 
+    ESP32.write(bytes([STREAM]))
+    print("starting streaming!")
     i = 0
     while i < N:
         raw = ESP32.read_until()
         try:
             x,y,z = dataParser(raw)
+            i += 1
+            time.sleep(sample_delay)
             measurements[i,0] = x
             measurements[i,1] = y
             measurements[i,2] = z
-            i += 1
+            ESP32.write(bytes([STREAM]))
         except:
             pass 
-    ESP32.Close()
-    print("Sensor Reading Complete")
     return measurements
 
 def dataLog(measurements,FILE):
     print('Writing data to {} ...'.format(FILE))
-    N = np.size(measurements)
+    N = int(np.size(measurements)/3)
     for i in range(N):
         with open(FILE, 'a', newline='') as f:
             writer = csv.writer(f, delimiter='\t')
             writer.writerow([measurements[i, 0], measurements[i, 1], measurements[i, 2]])
-    print("done")
 
+def dataplot(measurements,PIC):
+    print("plotting data")
+
+    
 def main():
     port = find_ESP32()
-    ESP32 = serial.Serial(port,baudrate=9600)
-    
+    ESP32 = serial.Serial(port,baudrate=115200)
+    handshake_ESP32(ESP32, handshake_code=HANDSHAKE, print_message=True)
 
+    #stream to get measurements
+    measurements = dataStream(ESP32)
+    print(measurements)
+    #log data and plot
+    dataLog(measurements,"raw.txt")
 if __name__ == "__main__":
     main()
